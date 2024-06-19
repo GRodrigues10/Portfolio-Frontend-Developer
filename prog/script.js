5c754654af8b30efcdde7d08a3c3854bcb02de03
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-06-18 21:01:53 -0300

    update

--- prog/script.js
+++ prog/script.js
@@ -5,3 +5,23 @@ document.getElementById('meuForm').addEventListener('submit', (event) => {
     submitBtn.disabled = true;
     loadingIndicator.style.display = 'block';
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const menuIcon = document.getElementById("menuIcon");
+    const menuHamburguer = document.getElementById("menuHamburguer");
+
+    menuIcon.addEventListener("click", () => {
+        if (menuHamburguer.classList.contains("show")) {
+            menuHamburguer.classList.remove("show");
+            setTimeout(() => {
+                menuHamburguer.style.display = "none";
+            }, 300); // Aguarda a transição terminar antes de ocultar o menu
+        } else {
+            menuHamburguer.style.display = "flex";
+            setTimeout(() => {
+                menuHamburguer.classList.add("show");
+            }, 10); // Adiciona a classe após definir display flex
+        }
+    });
+});
+
