39f78dc97f1549202dc4e853d58475ecec8ab46c
GabrieldaCostaRodrigues <167185964+GRodrigues10@users.noreply.github.com>
2024-07-19 19:47:05 -0300

    atualização

--- prog/script.js
+++ prog/script.js
@@ -68,8 +68,8 @@ function jogoDaVelha(){
     window.open('https://grodrigues10.github.io/Projeto-Jogo-Da-Velha/', 'blank')
 }
 
-function calc(){
-    window.open('https://grodrigues10.github.io/Projeto-Calculadora-ReactJS/', 'blank');
+function carrinho(){
+    window.open('https://grodrigues10.github.io/Projeto-Carrinho-de-Compras/', 'blank');
 }
 
 function clima(){
